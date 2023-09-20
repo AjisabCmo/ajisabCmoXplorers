cfd38a2f8d2e21f2c2b3f39081ead8a41db49b6e
Ismail Ajiboye <ajisab2014@gmail.com>
2023-09-20 07:00:08 +0100

    i can

---
+++ src/ChapterFour/FindingTwoLargest.java
@@ -0,0 +1,33 @@
+package ChapterFour;//package ChapterFour;
+
+import java.util.Scanner;
+
+public class FindingTwoLargest {
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int count = 0;
+       int number =0;
+      int largest1=0;
+       int largest2=largest1;
+
+       while (count<10){
+
+           System.out.println("enter the number: ");
+           number = scanner.nextInt();
+           count++;
+          if (number >largest1){
+              largest1=number ;
+               largest2=largest1;
+         } else if (number==largest2 && number !=largest1){
+              largest2=number;
+
+          }
+
+      }
+       System.out.println("the first largest is :"+largest1);
+       System.out.println("the second largest is :"+ largest2);
+   }
+}
+
+
+
